ef87e4be8a1f9b65d6d62255be4cbb24c8db3163
Simon Hildebrandt <simonhildebrandt@gmail.com>
2016-06-19 14:53:30 +1000

    Planets have dimensions

--- lib/planet.rb
+++ lib/planet.rb
@@ -1,3 +1,7 @@
 class Planet
+  attr_accessor :dimensions
 
+  def initialize(size)
+    @dimensions = size
+  end
 end
--- spec/planet_spec.rb
+++ spec/planet_spec.rb
@@ -2,5 +2,8 @@ require 'spec_helper'
 require 'planet'
 
 describe Planet do
+  subject { described_class.new 4 }
+
   it { should be_truthy }
+  it { expect(subject.dimensions).to be 4 }
 end
